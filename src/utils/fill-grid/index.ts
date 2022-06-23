ac7d560d24f9087564df3c656a2ef2e11295bd63
Jeff Andrews <babajeff8170@gmail.com>
2022-06-23 15:51:13 -0400

    adds function to identify current 3x3 square in grid and tests the functionality

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -2,60 +2,56 @@ import { GRID, NUMBERS } from "typings";
 import { shuffle, isInRow, isInCol } from "utils";
 
 const gridExample = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 /**
- * A recursive function to check all possible combinations of numbers until a solution found 
+ * A recursive function to check all possible combinations of numbers until a solution found
  * @param grid : ; a 9x9 grid for sudoku
  */
 function fillGrid(grid: GRID) {
-    let row = 0, col = 0;
+  let row = 0,
+    col = 0;
 
-    for(let i=0; i<81; i++){
-        row = Math.floor(i / 9);
-        col = i % 9;
-        if(grid[row][col] === 0) {
-            //do stuff
-            shuffle(numbers);
-            for(let value of numbers) {
-                //is it not in the grid row?
-                if(!isInRow({grid, row, value})) {
-                    // let isInCol = false;
-                    // for(let i = 0; i < 9; i++) {
-                    //     //is it not in the grid column?
-                    //     if(value === grid[i][col]) isInCol = true;
-                    // }
-                    if(!isInCol({grid, col, value})) {
-                        //is it not in the grid square?
-                        // if(!isInSquare())
-                    }
-
-                }
-                
-                
-                //if it meets none of these conditions put it in the square
-                grid[row][col] = value;
-                //if grid is full, we stop and return,
-                //otherwise, we run full grid again.
-            }
-            
-            //recursive things
-            break;
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+    if (grid[row][col] === 0) {
+      //do stuff
+      shuffle(numbers);
+      for (let value of numbers) {
+        //is it not in the grid row?
+        if (!isInRow({ grid, row, value })) {
+          if (!isInCol({ grid, col, value })) {
+            //is it not in the grid square?
+            const square = [
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+            ];
+          }
         }
+
+        //if it meets none of these conditions put it in the square
+        grid[row][col] = value;
+        //if grid is full, we stop and return,
+        //otherwise, we run full grid again.
+      }
+
+      //recursive things
+      break;
     }
+  }
 }
 
 export default fillGrid;
-
-
